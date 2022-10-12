4a6159bbfe4b67a4914c11ed5f2a334ca876201c
Saviour <owolabisaviour@gmail.com>
2022-10-12 13:49:37 +0100

    Add module for generating PLL clocks

---
+++ src/hdmi/hdmi_clocks.v
@@ -0,0 +1,27 @@
+//reference implementation is https://github.com/juj/HDMI_testikuva/blob/master/src/top.v + Gowin docs
+
+module hdmi_clock_gen (
+    input wire clk,
+    output reg hdmi_clk_5x,
+    output reg hdmi_clk
+);
+    
+
+//IMPLEMENT AS NECESSARY
+//IMPLEMENTATION HERE IS GOWIN SPECIFIC
+
+wire hdmi_clk_lock; 
+
+Gowin_rPLL this_clock_mult(
+    .clkin(clk), 
+    .clkout(hdmi_clk_5x)
+    );
+
+CLKDIV #(.DIV_MODE("5"), .GSREN("false")) hdmi_clock_div
+    (
+        .HCLKIN(hdmi_clk_5x),
+        .RESETN(hdmi_clk_lock),
+        .CALIB(1'b1),
+        .CLKOUT(hdmi_clk)
+    );
+endmodule
