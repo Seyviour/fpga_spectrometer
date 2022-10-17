567d3372713e29d2bf899627f4ef16af8495ed6f
Saviour <owolabisaviour@gmail.com>
2022-10-17 07:25:25 +0100

    UPDATE: output lock signal

--- src/hdmi/hdmi_clocks.v
+++ src/hdmi/hdmi_clocks.v
@@ -3,7 +3,8 @@
 module hdmi_clock_gen (
     input wire clk,
     output reg hdmi_clk_5x,
-    output reg hdmi_clk
+    output reg hdmi_clk,
+    output wire hdmi_clk_lock
 );
     
 
@@ -15,6 +16,7 @@ wire hdmi_clk_lock;
 Gowin_rPLL this_clock_mult(
     .clkin(clk), 
     .clkout(hdmi_clk_5x)
+    .lock(hdmi_clk_lock)
     );
 
 CLKDIV #(.DIV_MODE("5"), .GSREN("false")) hdmi_clock_div
