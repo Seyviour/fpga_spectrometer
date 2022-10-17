a4ad51a2588f1269ac6ad6d535842015dc4730a3
Saviour <owolabisaviour@gmail.com>
2022-10-17 15:11:11 +0100

    ADD sample index to address conversion module

--- src/idx_to_RAM.v
+++ src/idx_to_RAM.v
@@ -5,15 +5,18 @@ module idx2RAM #(
     FFT_SIZE = 256,
     NO_BANKS = 2
 ) (
-    input wire signed [$clog2(NO_FFTS)-1: 0] FFT_IDX,
-    input wire [$clog2(FFT_SIZE/2)-1:0] sample_idx,
+    input wire signed [6: 0] FFT_IDX,
+    input wire [6:0] sample_idx,
 
-    output reg [NO_BANKS-1: 0] bank_select, 
-    output reg [ADDRESS_WIDTH-1: 0] wr_address
+    output reg [1: 0] bank_select, 
+    output reg [11: 0] wr_address
 );
 
-localparam FFT_id_width = $clog2(NO_FFTS);
+localparam FFT_id_width = 6;
+
 reg [ADDRESS_WIDTH-1: 0] offset; 
+
+
 
 always @(*) begin
     case(FFT_IDX[FFT_id_width-1])
@@ -22,7 +25,7 @@ always @(*) begin
         default: bank_select = 2'b00;
     endcase
 
-    offset = FFT_IDX[FFT_id_width-2: 0] << 7; //128 LOCATIONS PER FFT
+    offset = {5'(FFT_IDX[FFT_id_width-2: 0]), 7'b0}; //128 LOCATIONS PER FFT
     wr_address = offset + sample_idx; 
 end
 
