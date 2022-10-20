ad878a651b4886b66774a7d235e146ea1ab0187a
Saviour <owolabisaviour@gmail.com>
2022-10-20 07:52:12 +0100

    UPDATE: switch to positive indexing for FFT_IDX

--- src/videogen/disp_mem_manager.v
+++ src/videogen/disp_mem_manager.v
@@ -1,5 +1,6 @@
 // `include "/home/saviour/study/fpga_spectrogram/src/videogen/pulse_detect_count.v"
 // `include "/home/saviour/study/fpga_spectrogram/src/videogen/mag_and_log.v"
+// `include "/home/saviour/study/fpga_spectrogram/src/videogen/idx_to_RAM.v"
 module dispManager #(
     parameter
     COUNTHIGH = 4410,
@@ -30,7 +31,11 @@ wire pulse;
 // wire disp_wr_en;
 //1 CYCLE DELAY 
 wire o_p_disp_wr_en;
+
 assign disp_wr_en = o_p_disp_wr_en && (addr_R<FFT_SIZE/2);
+
+
+
 pulseDetectCount #( .COUNTHIGH (COUNTHIGH )) thisPulseDetectCount 
     (
         .clk (clk),
@@ -57,55 +62,34 @@ always @(posedge clk)
 assign wr_idx = addr_R;
 
 
-// NEGATIVE INDEXING FOR OLDEST FFT_IDX
-// RANGE IS (-49 TO 0) ALL INCLUSIVE
-// STARTS WITH OLDEST AT -49
-// NEWEST IS AT -49 + 49
+
+// ** ASSUMING 50 FFTS (IDX 0 - 49)
+// DRAW ORDER IS FROM LEAST RECENT TO MOST RECENT DFT
+// WHEN OLDEST IS 49, 49 IS DRAWN FIRST AND 0 IS DRAWN LAST
+
+// WHEN OLDEST IS 0, 0 IS DRAWN FIRST AND 49 IS DRAWN LAST
+// AT RESET, 0 IS OLDEST AND 49 IS MOST RECENT
+
+localparam FFT_IDX_WIDTH = $clog2(NO_FFTS);
+
 
 always @(posedge clk) begin
-    if (pulse)
-        OLDEST_FFT_IDX <= OLDEST_FFT_IDX + 1'b1;
-    if (OLDEST_FFT_IDX == 0)
-        OLDEST_FFT_IDX <= -(NO_FFTS-1); 
-    if (reset)
-        OLDEST_FFT_IDX <= -(NO_FFTS-1);
-end
-
-localparam N_IDX_BITS = $clog2(NO_FFTS);
-
-wire [$clog2(NO_FFTS)-1:0] NEWEST_FFT_IDX;
-//reg [$clog2(NO_FFTS)-1:0]  CURR_FFT_IDX;
-
-assign NEWEST_FFT_IDX = OLDEST_FFT_IDX + (N_IDX_BITS)'(NO_FFTS-1); 
-
-//always @(*) begin
-//    CURR_FFT_IDX = ((NEWEST_FFT_IDX+y[COORDW-1:3]) > (N_IDX_BITS-1)'(NO_FFTS-1))
-//                    ? (NEWEST_FFT_IDX+y[COORDW-1:3]) - (N_IDX_BITS-1)'(NO_FFTS-1)
-//                    :  (NEWEST_FFT_IDX+y[COORDW-1:3]);            
-//end
-
-//idx2RAM #(.ADDRESS_WIDTH(12),.NO_FFTS(50 ),.FFT_SIZE(256 ),.NO_BANKS (2 )) thisidx2RAM
-//    (
-//        .FFT_IDX (NEWEST_FFT_IDX),
-//        .sample_idx (wr_idx),
-//        .bank_select (bank_select),
-//        .wr_address  (wr_address)
-//    );
-
-reg [11:0] offset; 
-always @(*) begin
-    case(NEWEST_FFT_IDX[N_IDX_BITS-1])
-        1'b0: bank_select = 2'b01; 
-        1'b1: bank_select = 2'b10;
-        default: bank_select = 2'b00;
-    endcase
-
-    offset = {5'(NEWEST_FFT_IDX[N_IDX_BITS-2: 0]), 7'b0}; //128 LOCATIONS PER FFT
-    wr_address = offset + wr_idx; 
+    if ((OLDEST_FFT_IDX == NO_FFTS-1) | reset) begin
+        OLDEST_FFT_IDX <= 0; 
+    end else begin 
+        if (pulse) 
+            OLDEST_FFT_IDX <= OLDEST_FFT_IDX + 1'b1;
+    end
 end
 
 
 
-
+idx2RAM #(.ADDRESS_WIDTH(ADDRESS_WIDTH),.NO_FFTS(NO_FFTS),.FFT_SIZE(FFT_SIZE),.NO_BANKS (NO_BANKS)) thisidx2RAM
+   (
+       .FFT_IDX (OLDEST_FFT_IDX), //write newest dft to location of oldest fft
+       .sample_idx (wr_idx),
+       .bank_select (bank_select),
+       .wr_address  (wr_address)
+   );
 
 endmodule
