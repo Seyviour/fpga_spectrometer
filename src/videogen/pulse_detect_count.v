2c6b3bebc41cd2e3d2fd107192f41cf644b767e7
Saviour <owolabisaviour@gmail.com>
2022-10-27 09:45:59 +0100

    UPDATE: Count pulses on negative edge of wr_en signal

--- src/videogen/pulse_detect_count.v
+++ src/videogen/pulse_detect_count.v
@@ -2,33 +2,53 @@
 
 module pulseDetectCount #(
     parameter
-    COUNTHIGH = 4410
+    COUNTHIGH = 20
 
 ) (
     input wire clk,
     input wire reset, 
     input wire wr_en, 
-    output wire disp_wr_en,
-    output wire pulse
+    output reg count_true,
+    output reg pulse
 );
-
+// On a negative pulse of the wr_en signal
+// count increment
     reg [$clog2(COUNTHIGH)-1: 0] count; 
-    reg wr_en_R;
+    reg wr_en_R1;// wr_en_R2;
 
 
     always @(posedge clk) begin 
-        if (pulse)
-            count <= count + 1'b1;
-        if (reset)
-            count <= 0; 
+        if (reset) begin
+            count <= 0;
+            count_true <= 1'b0;
+        end else begin
+            if (pulse) begin
+                if (count == COUNTHIGH-1) begin
+                    count_true <= 1'b1; 
+                    count <= 0;
+                end else begin
+                    count_true <= 1'b0; 
+                    count <= count + 1'b1; 
+                end
+            end   
+        end
     end
 
     always @(posedge clk) begin
-        wr_en_R <= wr_en; 
+        wr_en_R1 <= wr_en;
     end
 
-    assign pulse = ~wr_en_R && wr_en;
+    always @(*)
+        pulse =  wr_en_R1 && ~wr_en;
 
-    assign disp_wr_en = (count == COUNTHIGH-1) && wr_en_R; 
+    // assign pulse =
+    // assign count_true = (count == COUNTHIGH-1)? 1'B1:1'B0; 
+
+    // initial begin
+    //     $dumpfile("pulseDetectCount.vcd");
+    //     $dumpvars(0, pulseDetectCount);
+    //     #1;
+    // end
+
 
 endmodule
