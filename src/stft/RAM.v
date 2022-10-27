ec6feb329294ec4b50bcdee3a655602629b84467
Saviour <owolabisaviour@gmail.com>
2022-10-27 09:23:16 +0100

    UPDATE: Correction to RAM array declaration

--- src/stft/RAM.v
+++ src/stft/RAM.v
@@ -8,15 +8,14 @@ module iRAM #(
     input wire [ADDRESS_WIDTH-1: 0] wr_addr,
     input wire [ADDRESS_WIDTH-1: 0] rd_addr,
     input wire [WORD_WIDTH-1: 0] wr_data,
-    output wire [WORD_WIDTH -1: 0] rd_data
+    output bit [WORD_WIDTH -1: 0] rd_data
 );
     
-// reg [WORD_WIDTH-1: 0] RAM [2**ADDRESS_WIDTH-1: 0];
-reg [2**ADDRESS_WIDTH-1: 0] RAM [WORD_WIDTH-1: 0];
-
+ reg [WORD_WIDTH-1: 0] RAM [2**ADDRESS_WIDTH-1: 0];
+//reg [2**ADDRESS_WIDTH-1: 0] RAM [WORD_WIDTH-1: 0];
+ 
 always @(posedge clk)
     if (wr_en)
         RAM[wr_addr] <= wr_data; 
-
 assign rd_data = RAM[rd_addr];
 endmodule
