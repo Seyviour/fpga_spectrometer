22a0a3832cefcf75a070a7023c301167576986d6
Saviour <owolabisaviour@gmail.com>
2022-10-27 09:19:32 +0100

    UPDATE: register signals to improve timing

--- src/sharedmemory/coord_to_ram.v
+++ src/sharedmemory/coord_to_ram.v
@@ -4,67 +4,65 @@
 module coord_to_ram #(
     parameter
     NO_BANKS = 2,
-    COORDW = 10,
+    COORDW = 16,
     RAM_ADDR_WIDTH = 12,
-    NO_FFTS = 50
+    NO_FFTS = 50,
+    FFT_SIZE = 256
 ) (
-    input wire [COORDW-1: 0] x,
-    input wire [COORDW-1: 0] y,
-    input wire signed [$clog2(NO_FFTS)-1:0] OLDEST_FFT_IDX,
+    input wire clk, 
+    input wire [COORDW-1: 0] x, // 0 - 512
+    input wire [COORDW-1: 0] y, // 0 - 
+    input wire signed [$clog2(NO_FFTS)-1:0] OLDEST_FFT_IDX, 
 
     output reg [NO_BANKS-1: 0] rd_bank_select, 
     output reg [RAM_ADDR_WIDTH-1: 0] rd_address
 );
 
-localparam N_IDX_BITS = $clog2(NO_FFTS);
 
-wire [$clog2(NO_FFTS):0] NEWEST_FFT_IDX;
-reg [$clog2(NO_FFTS)-1:0]  CURR_FFT_IDX;
+// There are two banks of RAM, each 4Kx4bits
+// For each FFT_ID (0-50), the bank will be selected as the MSB
 
-assign NEWEST_FFT_IDX = OLDEST_FFT_IDX + (N_IDX_BITS-1)'(NO_FFTS-1); 
+// Scanning horizontally, each pixel should be repeated 4 times
+// Every FFT has 128 elements (mirror eliminated from 256)
+// The last two bits of the x-component of the address are disregarded
+// Each "pixel" is also repeated along the vertical 4 times
+
+reg [COORDW-1: 0] i_x;
+reg [COORDW-1: 0] i_y;
+
+always @(posedge clk) begin 
+    i_x <= COORDW'(x + 1);
+    i_y <=  y; 
+end
+
+localparam FFT_IDX_WIDTH = $clog2(NO_FFTS);
+localparam FFT_IDX_MSB = FFT_IDX_WIDTH-1; 
+
+reg [FFT_IDX_WIDTH:0] IDX_SUM;
+reg [FFT_IDX_WIDTH-1:0] CURR_FFT_IDX;
 
 reg [RAM_ADDR_WIDTH-1: 0] offset; 
 
+always @(posedge clk) begin
 
-// always @(*) begin
-//     case(y[COORDW-1: COORDW-4]) //bank select is upper 4 bits
-//         4'b0000: rd_bank_select = 8'b00000001;
-//         4'b0001: rd_bank_select = 8'b00000010;
-//         4'b0010: rd_bank_select = 8'b00000100;
-//         4'b0011: rd_bank_select = 8'b00001000;
-//         4'b0100: rd_bank_select = 8'b00010000;
-//         4'b0101: rd_bank_select = 8'b00100000;
-//         4'b0110: rd_bank_select = 8'b01000000;
-//         4'b0111: rd_bank_select = 8'b10000000;
-//         default: rd_bank_select = x; 
-//     endcase
-// end
-
-always @(*) begin
-    CURR_FFT_IDX = ((NEWEST_FFT_IDX+y[COORDW-1:3]) > (N_IDX_BITS-1)'(NO_FFTS-1))
-                    ? (NEWEST_FFT_IDX+y[COORDW-1:3]) - (N_IDX_BITS-1)'(NO_FFTS-1)
-                    :  (NEWEST_FFT_IDX+y[COORDW-1:3]);            
+    IDX_SUM <= OLDEST_FFT_IDX + i_y[4+:FFT_IDX_WIDTH];
+    if (IDX_SUM <= (NO_FFTS-1))
+        CURR_FFT_IDX <= IDX_SUM;
+    else
+        CURR_FFT_IDX <= IDX_SUM - NO_FFTS; 
 end
 
-always @(*) begin
+always @(posedge clk) begin
 //    case(CURR_FFT_IDX[$clog2(NO_FFTS)-1])    
-    case(CURR_FFT_IDX[2])
-
-
-        1'b0: rd_bank_select = 2'b01;
-        1'b1: rd_bank_select = 2'b10;
+    case(CURR_FFT_IDX[FFT_IDX_WIDTH-1])
+        1'b0: rd_bank_select <= 2'b01;
+        1'b1: rd_bank_select <= 2'b10;
         default: rd_bank_select = 2'b00;
     endcase
 
-    offset = {CURR_FFT_IDX [(N_IDX_BITS-2) : 0], 7'b0};
-    rd_address = offset + x[COORDW-1:3];
+    offset <= {CURR_FFT_IDX[(FFT_IDX_WIDTH-2) : 0], ($clog2(FFT_SIZE/2))'(0)}; //TODO: parameterize this 7!
+    rd_address <= offset + i_x[COORDW-1:2];
 
 end
-
-// always @(*) begin
-//     offset = {y[5:3], {9{1'b0}}};
-//     rd_address = offset + x; 
-// end
-
     
 endmodule
