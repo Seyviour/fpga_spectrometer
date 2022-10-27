2a8b888520ec941d58c0b7bf495540f148e957fa
Saviour <owolabisaviour@gmail.com>
2022-10-27 09:22:07 +0100

    UPDATE factors for 256-size DFT/STFT

--- src/stft/factor_gen.py
+++ src/stft/factor_gen.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     str_mag_format = "{:04x}"
     multiplier = 2 ** (16-1)-1 
     with open ("factor.txt", "w") as f:
-        for x in range(512):
-            factor = np.exp(-1j*2*np.pi*x/512) * multiplier
+        for x in range(256):
+            factor = np.exp(-1j*2*np.pi*x/256) * multiplier
             real = int(factor.real) & (2**16)-1
             imag = int(factor.imag) & (2**16)-1
 
