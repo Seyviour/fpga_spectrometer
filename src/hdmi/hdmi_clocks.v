457ad5dcc46e15f480a1677f3067238299b119d5
Saviour <owolabisaviour@gmail.com>
2022-10-29 09:15:21 +0100

    UPDATE: Optionally generate clocks (in simulation) when SIM parameter is set

--- src/hdmi/hdmi_clocks.v
+++ src/hdmi/hdmi_clocks.v
@@ -1,29 +1,60 @@
 //reference implementation is https://github.com/juj/HDMI_testikuva/blob/master/src/top.v + Gowin docs
+`timescale 1 ns/10 ps
+module hdmi_clock_gen #(
+    parameter
+    SIM=1
+) 
 
-module hdmi_clock_gen (
+(
     input wire clk,
     output reg hdmi_clk_5x,
     output reg hdmi_clk,
     output wire hdmi_clk_lock
 );
     
+localparam CLK_25_2_PERIOD = 40ns;
+localparam CLK_126_PERIOD = 8ns; 
+
+
+reg hdmi_clk_5x_i, hdmi_clk_i; 
+
 
 //IMPLEMENT AS NECESSARY
 //IMPLEMENTATION HERE IS GOWIN SPECIFIC
 assign hdmi_clk_lock = hdmi_clk_lock_i;
 wire hdmi_clk_lock_i; 
 
-Gowin_rPLL this_clock_mult(
-    .clkin(clk), 
-    .clkout(hdmi_clk_5x),
-    .lock(hdmi_clk_lock_i)
-    );
+generate
+if (!SIM) begin   
 
-CLKDIV #(.DIV_MODE("5"), .GSREN("false")) hdmi_clock_div
-    (
-        .HCLKIN(hdmi_clk_5x),
-        .RESETN(hdmi_clk_lock_i),
-        .CALIB(1'b1),
-        .CLKOUT(hdmi_clk)
-    );
+    Gowin_rPLL this_clock_mult(
+        .clkin(clk), 
+        .clkout(hdmi_clk_5x),
+        .lock(hdmi_clk_lock_i)
+    ); 
+
+    
+    CLKDIV #(.DIV_MODE("5"), .GSREN("false")) hdmi_clock_div
+        (
+            .HCLKIN(hdmi_clk_5x),
+            .RESETN(hdmi_clk_lock_i),
+            .CALIB(1'b1),
+            .CLKOUT(hdmi_clk)
+        );
+    end 
+ endgenerate
+
+ if(SIM) begin
+    assign hdmi_clk_5x = hdmi_clk_5x_i;
+    assign hdmi_clk = hdmi_clk_i;
+    always # (CLK_126_PERIOD/2) hdmi_clk_5x_i = ~hdmi_clk_5x_i;
+    always # (CLK_25_2_PERIOD/2) hdmi_clk_i = ~hdmi_clk_i; 
+
+end
+
+initial begin
+    hdmi_clk_5x_i = 1'b0;
+    hdmi_clk_i = 1'b0;  
+end 
+
 endmodule
--- src/i2s/i2s_clk_gen.v
+++ src/i2s/i2s_clk_gen.v
@@ -1,22 +1,42 @@
-module i2s_clk_gen (
+`timescale 1 ns/10 ps
+
+module i2s_clk_gen #(
+    parameter
+    SIM=0
+)(
     input wire clk_in,
     input wire reset, 
     output wire i2s_clk
 );
     //VENDOR SPECIFIC
-
+reg i2s_clk_i; 
 wire i2s_clk_8x;
-wire lock; 
-rpll_i2s_clk_8x i2s_clk_8x_m(
-        .clkout(i2s_clk_8x), //output clkout
-        .clkin(clk_in), //input clkin
-        .lock_o(lock)
-    );
+wire lock;
+localparam i2s_SIM_PERIOD = 160;
 
-gowin_i2s_clk_div_8 clk_div_8(
-    .clkout(i2s_clk), //output clkout
-    .hclkin(i2s_clk_8x), //input hclkin
-    .resetn(lock) //input resetn
-);
+generate
+if (!SIM) begin 
+    rpll_i2s_clk_8x i2s_clk_8x_m(
+            .clkout(i2s_clk_8x), //output clkout
+            .clkin(clk_in), //input clkin
+            .lock_o(lock)
+        );
+
+    gowin_i2s_clk_div_8 clk_div_8(
+        .clkout(i2s_clk), //output clkout
+        .hclkin(i2s_clk_8x), //input hclkin
+        .resetn(lock) //input resetn
+    );
+    end 
+endgenerate
+
+if (SIM) begin
+    assign i2s_clk = i2s_clk_i;
+    always #(i2s_SIM_PERIOD/2) i2s_clk_i = ~i2s_clk_i; 
+end
+
+initial begin
+    i2s_clk_i = 0; 
+end
 
 endmodule
